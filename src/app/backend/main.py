245387e3892bc3615921b697fa2bac26d4ca9142
RealGenius1 <sptho24@gmail.com>
2025-10-06 12:01:43 -0400

    oops

--- src/app/backend/main.py
+++ src/app/backend/main.py
@@ -1,9 +1,6 @@
 from fatsecret import Fatsecret
 import json
 
-consumerKey = '9015618b120842939c12b10f9dc4b552'
-consumerSecret = 'e0c9ccfea56942528e14630b0c8b0b8c'
-
 fs = Fatsecret(consumer_key=consumerKey, consumer_secret=consumerSecret)
 
 foods = fs.foods_search("Chicken")
