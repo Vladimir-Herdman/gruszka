08a0bd59ec513ef1db1178ac65b497fd646197d4
RealGenius1 <sptho24@gmail.com>
2025-10-06 12:17:29 -0400

    OK i did fix things
    Python environment instructions work, you might need to make sure it actually runs the right environment, but it does exist.

--- src/app/backend/main.py
+++ src/app/backend/main.py
@@ -1,5 +1,13 @@
 from fatsecret import Fatsecret
 import json
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+consumerKey = os.environ.get('consumerKey')
+consumerSecret = os.environ.get('consumerSecret')
 
 fs = Fatsecret(consumer_key=consumerKey, consumer_secret=consumerSecret)
 
